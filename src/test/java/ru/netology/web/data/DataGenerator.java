25274d8c52574e941e5fb7213e784df464c389ca
Alex Norin <aleksey43n@gmail.com>
2022-05-18 21:53:41 +0300

    initial commit

---
+++ src/test/java/ru/netology/web/data/DataGenerator.java
@@ -0,0 +1,36 @@
+package ru.netology.web.data;
+
+import com.github.javafaker.Faker;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    public static class UserInfo {
+        private final String city;
+        private final String name;
+        private final String phone;
+        private final String date;
+        private final String dateRescheduling;
+    }
+
+    public static UserInfo getUserInfo() {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        Faker faker = new Faker(new Locale("ru"));
+        return new UserInfo(faker.address().city(),
+                faker.name().fullName(),
+                faker.phoneNumber().phoneNumber(),
+                LocalDate.now().plusDays(3).format(df),
+                LocalDate.now().plusDays(5).format(df));
+    }
+}
+
